4b851ae2d029f6d348b91515e4d98f1dae685879
Evan <evan.li@payrange.com>
2026-05-18 11:42:57 +0800

    业务重构

--- routers/zct.py
+++ routers/zct.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import logging
 from pathlib import Path
+from typing import Optional
 
 from fastapi import APIRouter, Body, Depends, HTTPException, Query
 from fastapi.responses import HTMLResponse
